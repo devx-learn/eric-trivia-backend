e230a529445cb18587f749f9ba4558406181007a
Rob Thuleen <rob.thuleen@gmail.com>
2018-02-20 19:56:25 -0800

    Merge index.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const app = require('./app')
 
-<<<<<<< HEAD
 app.listen(3000, function() {
  console.log('Trivia Server listening on port 3000!');
-=======
-app.listen(3000, function () {
- console.log('Trivia Server listening on port 3000! o shit whaddup');
->>>>>>> master
 });
